0ce9cd979a18303c086afc5b14d79397b532e8e5
Haley Barlar <hbarlar1@gmail.com>
2018-08-05 21:58:58 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
   alphabet = ["abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"]
   new = []
   alphabet.split(' ').collect do |i|
-    new << 
+    new << alphabet(i)
   arr.sort_by do |word|
     word.split(' ').collect do |x|
       alphabet.index(x)
